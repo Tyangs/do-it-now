6cff32c28bfbb42dd61c4339d2c4aac6d5c9a4cb
t.yang <t.yang@tyang-Mac.local>
2022-06-29 23:36:37 +0800

    :heavy_plus_sign: (react-router) add react-router config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
+
+const getDirPath = (dir: string) => path.resolve(__dirname, dir);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    open: true,
+  },
+  resolve: {
+    alias: {
+      '@': getDirPath('src'),
+    },
+  },
 });
