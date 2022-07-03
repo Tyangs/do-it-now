961d2a611ab1a5424ad5d4aea757b35149770507
t.yang <t.yang@tyang-Mac.local>
2022-07-03 23:19:53 +0800

    :heavy_plus_sign: (add-deps) add deps for ts

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import Intro from './components/Intro';
-import Layout from './components/Layout/Layout';
+import Layout from './components/Layout';
 
 const Login = () => {
   return <Layout intro={<Intro />} signIn="" />;
